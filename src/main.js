cc50ff308a183fab2dbc60d3bcd4926ccce1139c
2t1b <146372402+2t1b@users.noreply.github.com>
2023-10-18 18:20:50 +0000

    save commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const notesContainer = document.getElementById("app");
 const addNoteButton = notesContainer.querySelector(".add-note");
-
 getNotes().forEach((note) => {
   const noteElement = createNoteElement(note.id, note.content);
   notesContainer.insertBefore(noteElement, addNoteButton);
